6b9c3770358539b07b49b04675b3af6770792aba
NATHAN LAMONT SMITH <andinto@gmail.com>
2019-03-01 16:04:47 -0600

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Nav from './Nav.jsx';
 import Footer from './Footer.jsx';
@@ -7,13 +6,50 @@ import Content from './Content.jsx';
 import Authorization from './Authorization.jsx';
 import styled from 'styled-components'
 
+// need this to be smart, putting in some stuff we might want: 
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      loggedIn: false,
+      username: "",
+      userId: "",
+      admin: false,
+      pathId: "",
+      page: "home",
+      clickedImages: [],
+      authView: "reg"  // --> "reg" -- displays SIGN UP, "login" -- displays LOG IN
+    }
+  }
+  goTo = (destination) => {
+    this.setState({
+      page: destination
+    })
+  }
+  register = (formData) => {
+    console.log("Register function fired! formData: ")
+    console.log(formData)
+  }
+  login = (formData) => {
+    console.log("Login function fired! formData: ")
+    console.log(formData)
+  }
+  setAuthView = (authView) => {
+    // acceptable inputs: "reg", "login"
+    this.setState({
+      authView: authView 
+    })
+  }
+  getImages = (imageId = null) => {
+    // need to communicate w/ database; if no imageId is supplied, default value is null, 
+    // which should get totally random images for initial layout 
+  }
   render() {
     return (
       <div className="App">
         <Nav />
-        <Authorization />
+        <Authorization authView={this.state.authView} register={this.register} login={this.login} />
         <Footer />
       </div>
     );
