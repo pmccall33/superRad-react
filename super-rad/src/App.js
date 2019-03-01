f6a63cdb8e7426da4822af20905de924da97e3a9
NATHAN LAMONT SMITH <andinto@gmail.com>
2019-02-28 19:07:49 -0600

    added logic for authorization, still need to connect to backend

--- super-rad/src/App.js
+++ super-rad/src/App.js
@@ -7,13 +7,50 @@ import Content from './Content.jsx';
 import Authorization from './Authorization.jsx';
 import styled from 'styled-components'
 
+// need this to be smart, putting in some stuff we might want: 
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      loggedIn: false,
+      username: "",
+      userId: "",
+      admin: false,
+      pathId: "",
+      page: "home",
+      clickedImages: [],
+      authView: "reg"  // --> "reg" -- displays SIGN UP, "login" -- displays LOG IN
+    }
+  }
+  goTo = (destination) => {
+    this.setState({
+      page: destination
+    })
+  }
+  register = (formData) => {
+    console.log("Register function fired! formData: ")
+    console.log(formData)
+  }
+  login = (formData) => {
+    console.log("Login function fired! formData: ")
+    console.log(formData)
+  }
+  setAuthView = (authView) => {
+    // acceptable inputs: "reg", "login"
+    this.setState({
+      authView: authView 
+    })
+  }
+  getImages = (imageId = null) => {
+    // need to communicate w/ database; if no imageId is supplied, default value is null, 
+    // which should get totally random images for initial layout 
+  }
   render() {
     return (
       <div className="App">
         <Nav />
-        <Authorization />
+        <Authorization authView={this.state.authView} register={this.register} login={this.login} />
         <Footer />
       </div>
     );
--- super-rad/src/Authorization.jsx
+++ super-rad/src/Authorization.jsx
@@ -8,20 +8,73 @@ const StyledAuth = styled.div`
   margin-bottom: 100px;
 `
 
+// Adding user authorization stuff: 
+
 class Authorization extends Component {
+  constructor(props){
+    super();
+
+    let authView = props.authView 
+    if (props.authView !== "reg" || props.authView !== "login") {
+      authView = "reg"
+    } 
+
+    this.state = {
+      username: "",
+      password: "",
+      confirmation: "",
+      message: "",
+      authView: authView
+    }
+  }
+  handleChange = (evt) => {
+    this.setState({
+      [evt.currentTarget.name]: evt.currentTarget.value 
+    })
+  }
+  toggleView = (view) => {
+    this.setState({
+      authView: view
+    })
+  }
+  submit = (type) => {
+    if (!this.state.username || !this.state.password) {
+      this.setState({
+        username: "",
+        password: "", 
+        confirmation: "",
+        message: "Invalid username and/or password"
+      })
+    } else if (type === "reg" && this.state.password !== this.state.confirmation) {
+      this.setState({
+        username: "",
+        password: "", 
+        confirmation: "",
+        message: "Password must match 'confirm password'"
+      })
+    } else {
+      type === "reg" ? this.props.register(this.state) : this.props.login(this.state)
+    }
+  }
   render(){
     return(
       <div>
+        { this.state.message ? <h4>{this.state.message}</h4> : <h4> &nbsp; </h4> }
         <StyledAuth>
           <form>
-            <h1>Log In</h1>
-            <input type="text" placeholder="username"></input><br/>
-            <input type="password" placeholder="password"></input>
+            { this.state.authView === "login" ? <h1>Log In</h1> : null }
+            { this.state.authView === "login" ? <button onClick={this.toggleView.bind("reg")}> Make New Account </button> : null }
+            { this.state.authView === "login" ? <input onChange={this.handleChange} type="text" name="username" value={this.state.username} placeholder="username"></input> : null}
+            { this.state.authView === "login" ? <input onChange={this.handleChange} type="password" name="password" value={this.state.password} placeholder="password"></input> : null}
+            { this.state.authView === "login" ? <button onClick={this.submit("login")}>Log In</button> : null }
           </form>
           <form>
-            <h1>Sign Up</h1>
-            <input type="text" placeholder="username"></input><br/>
-            <input type="password" placeholder="password"></input>
+            { this.state.authView === "reg" ? <h1>Sign Up</h1> : null }
+            { this.state.authView === "reg" ? <button onClick={this.toggleView.bind("login")}> Log In </button>: null }
+            { this.state.authView === "reg" ? <input onChange={this.handleChange} type="text" name="username" value={this.state.username} placeholder="username"></input> : null }
+            { this.state.authView === "reg" ? <input onChange={this.handleChange} type="password" name="password" value={this.state.password} placeholder="password"></input> : null }
+            { this.state.authView === "reg" ? <input onChange={this.handleChange} type="password" name="confirmation" value={this.state.confirmation} placeholder="confirm password"></input> : null }
+            { this.state.authView === "reg" ? <button onClick={this.submit("reg")}>Create Account</button> : null }
           </form>
         </StyledAuth>
       </div>
